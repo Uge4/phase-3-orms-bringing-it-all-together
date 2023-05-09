0b36cd3109f514fd55de41780ad1f90b2e1f45f4
eugene <eugeneholenstein@gmail.com>
2023-05-09 21:41:21 +1000

    commit tree b368b4f4c3cefca0ed06071b5113918461b3029c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+
+
 class Dog
 
+    attr_accessor :name, :breed
+    def initialize(attributes)
+        @id = attributes[:id]
+        @name = attributes[:name]
+        @breed = attributes[:breed]
+        
+    end
+
 end
+
+
+phil = Dog.new(name:"Bill", breed:"Poodle")
+puts phil.name
+
